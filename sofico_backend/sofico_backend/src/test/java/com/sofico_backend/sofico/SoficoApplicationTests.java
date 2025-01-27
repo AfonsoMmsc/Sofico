f52d4ee9ca741a1726bc469a583ddd7c01857b51
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2025-01-27 10:42:44 +0000

    Initialize Sofico backend project with Maven and Spring Boot setup

---
+++ sofico_backend/sofico_backend/src/test/java/com/sofico_backend/sofico/SoficoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofico_backend.sofico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoficoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
